ef4f066ac37eb722fba539c9bb84e28e6bd198e5
Vikas Koladiya <114695020+VikasKoladiya@users.noreply.github.com>
2022-11-07 19:38:29 +0530

    Add files via upload

---
+++ C-Language/sayllabus/PR1_2.C
@@ -0,0 +1,36 @@
+/*
+Name:Koladiya Vikas B.
+ID  :22TDCE033
+Prac:practical 1.2
+*/
+#include<stdio.h>
+//#include<conio.h>
+int main()
+{
+	//clrscr();
+
+		printf("\n\n#=====#====#=====#=====#=====#=====#=====#======#=====#=====#=====#=====#=====#\n");
+		printf("\t\t\t\t  BIO-DATA");
+		printf("\n#=====#====#=====#=====#=====#=====#=====#======#=====#=====#=====#=====#=====#\n");
+		printf("\t\t\t\tBasic Information");
+		printf("\n\t\t\t\t-----------------");
+		printf("\n\t\t\tNAME\t\t:\tKoladiya Vikas ");
+		printf("\n\t\t\tADDRESS\t\t:\tV-3,134 Swati Soc.,\n\t\t\t\t\t\tNana Varachha,surat.");
+		printf("\n\t\t\tMOBILE NUMBER\t:\t9265140223");
+		printf("\n\t\t\tGENDER\t\t:\tMale");
+		printf("\n\t\t\tDATE OF BIRTH\t:\t31-07-2005");
+		printf("\n\n\t\t\t\tEducation Qualification");
+		printf("\n\t\t\t\t-----------------------");
+		printf("\n\t\t\tSSC  ->  Tapovan vidhyalay  ->  2020  ->  \"85.50\"");
+		printf("\n\t\t\tHSC  ->  Tapovan vidhyalay  ->  2022  ->  \"79.00\"");
+		printf("\n\n\t\t\t\tOther information");
+		printf("\n\t\t\t\t-----------------");
+		printf("\n\t\tTECHNICAL SKILLS\t:\tC,C++,html,css,java");
+		printf("\n\t\t\tHOBBIES\t\t:\tcoding,cricket,photo Editting,Graphics");
+		printf("\n\n#=====#====#=====#=====#=====#=====#=====#======#=====#=====#=====#=====#=====#\n");
+		printf("\t\t\t\tTHANK YOU");
+		printf("\n#=====#====#=====#=====#=====#=====#=====#======#=====#=====#=====#=====#=====#");
+	//getch();
+	return 0;
+
+}
