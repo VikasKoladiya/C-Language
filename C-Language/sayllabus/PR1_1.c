14d2cac8d651fd673cf98b189428b565991b42b9
Vikas Koladiya <114695020+VikasKoladiya@users.noreply.github.com>
2022-11-07 19:36:04 +0530

    Add files via upload

---
+++ C-Language/sayllabus/PR1_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+	int a=1,b=4,c=3;
+	clrscr();
+	printf("\n\n\t\t\t\t  ID : 22TDCS033");
+	printf("\n\n%c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c",a,b,c,a,b,c,a,b,c,a,b,c,a,b,c,a,b,c,a,b,c,a,b,c,a,b,c,a,b,c,a,b,c,a,b,c,a,b,c,a);
+	printf("\n%c\"If you are resisting something, you are feeding it.\t\t\t      %c",b,b);
+	printf("\n%c\t\tAny energy you fight you are feeding it.\t\t      %c",c,c);
+	printf("\n%c\t\t\tIf you are pushing something away,\t\t      %c",a,a);
+	printf("\n%c\t\t\t       you are inviting it to stay.\"by michael singer %c",b,b);
+	printf("\n%c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c",c,a,b,c,a,b,c,a,b,c,a,b,c,a,b,c,a,b,c,a,b,c,a,b,c,a,b,c,a,b,c,a,b,c,a,b,c,a,b,c);
+	getch(); }
