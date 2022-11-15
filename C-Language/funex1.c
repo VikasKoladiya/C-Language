b591ecf44cfd411842a8dd84c223f8cf6a7c5384
Vikas Koladiya <114695020+VikasKoladiya@users.noreply.github.com>
2022-11-15 16:04:41 +0530

    Add files via upload

---
+++ C-Language/funex1.c
@@ -0,0 +1,90 @@
+#include<stdio.h>
+#include<conio.h>
+/*void dosqar()
+{
+    int a;
+    printf("Enter number a: ");
+    scanf("%d",&a);
+}
+void doqube()
+{
+    int a;
+    printf("\nEnter number a : ");
+    scanf("%d",&a);
+}
+void main()
+{
+    int a,b,sqar,q;
+    dosqar();
+    doqube();
+    printf("\n %d^2 : %d",a,a*a);
+    printf("\n %d^3 : %d",a,a*a*a);
+}
+
+
+// with return - with return
+/*int dosqr(int a)
+{
+    return a*a;
+}
+int doqub(int b)
+{
+    return b*b*b;
+}
+void main()
+{
+    int a,s,q,b;
+    printf("ENter a number : ");
+    scanf("%d",&a);
+    printf("Enter number b : ");
+    scanf("%d",&b);
+    s=dosqr(a);
+    q=doqub(b);
+
+    printf("%d^2 : %d",a,s);
+    printf("\n%d^3 : %d",b,q);
+}*/
+
+
+// no return - with perameters
+/*void dosqr(int a)
+{
+    printf("\n a^2 : %d",a*a);
+}
+void doqub(int a)
+{
+    printf("\n a^3 : %d",a*a*a);
+}
+void main()
+{
+    int a;
+    printf("Enter number : ");
+    scanf("%d",&a);
+    dosqr(a);
+    doqub(a);
+    printf("\nfunction end");
+}*/
+
+
+// no perameter - with return
+/*int dosqr()
+{
+    int a;
+    printf("Enter number : ");
+    scanf("%d",&a);
+    return a*a;
+} 
+int doqub()
+{
+    int a;
+    scanf("%d",&a);
+    return a*a*a;
+}
+void main()
+{
+    int s,q;
+    s=dosqr();
+    q=doqub();
+    printf("\n a^2 : %d",s);
+    printf("\n a^3 : %d",q);
+}*/
