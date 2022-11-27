fb0e2122f82b6e440f38e0c421857b6b59dc4ed5
Vikas Koladiya <114695020+VikasKoladiya@users.noreply.github.com>
2022-11-28 00:04:42 +0530

    Add files via upload

---
+++ C-Language/sum.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{    
+// a first rite sum lakhi sakay
+   int a=10,b=20,z;
+   int a1=10,b1=34,z1;
+   printf("a*b : %d", a*b);
+   printf("\na+b : %d",a+b);
+   printf("\na-b :%d",a-b);
+
+// aa second rite sum lakhi sakay (ek samikaran banavi ne)   
+   z=a/b;
+   printf("\nz : %d",z);
+
+// ek karta vadhare samikaran banava hoi to z ni jagayaye z1 ,a1,b1 lakhi pachi same method thi lakhvanu  
+   z1=a1/b1;
+   printf("\nz1 : %d",z1); 
+}
