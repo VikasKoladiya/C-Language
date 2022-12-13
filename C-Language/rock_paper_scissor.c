f0e2cd0fbdd415347999502c81e52d7dbe95146e
Vikas Koladiya <114695020+VikasKoladiya@users.noreply.github.com>
2022-12-13 23:46:15 +0530

    Add files via upload

---
+++ C-Language/rock_paper_scissor.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+void main()
+{
+    int com;
+    char your;
+    srand(time(NULL));
+    printf("you can press r for rock : \n");
+    printf("you can press p for paper : \n");
+    printf("you can press s dor scissor : \n");
+    while(1)
+    {
+       
+        com = rand()%3;
+        printf("\nyour turn : ");
+        scanf("%c",&your);
+        fflush(stdin);
+        if(your=='r' || your=='p' || your=='s' || your=='R' || your=='P' || your=='S')
+        {
+            if((your=='r' || your=='R') && com==1)
+            {
+                printf("\ncomputer select paper and you have select rock");
+                printf("\nyou lose this turn.");
+            }
+            
+            if((your=='r' || your=='R') && com==2)
+            {
+                printf("\ncomputer select scissor and you have select rock");
+                printf("\nyou won this turn.");
+            }
+
+            
+            if((your=='r' || your=='R') && com==0)
+            {
+                printf("\ncomputer select rock and you have select rock");
+                printf("\nthis turn is tie...continue....");
+            }
+
+            
+            if((your=='p' || your=='P') && com==0)
+            {
+                printf("\ncomputer select rock and you have select paper");
+                printf("\nyou won this turn.");
+            }
+            
+            if((your=='p' || your=='P') && com==1)
+            {
+                printf("\ncomputer select paper and you have select paper");
+                printf("\nthis turn is tie...continue...");
+            }
+            
+            if((your=='p' || your=='P') && com==2)
+            {
+                printf("\ncomputer select scissor and you have select paper");
+                printf("\nyou lose this turn.");
+            }
+            
+            if(your=='s' || your=='S')
+            {
+                printf("\nyou have select scissor so game is over!!!");
+                break;
+            }
+        }
+        else
+        {
+            printf("\nYou have select wrong option!!\n");
+        }
+    }
+}
