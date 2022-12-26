7de5ebde3088372344fd7dd75e144eb8c9791168
Vikas Koladiya <114695020+VikasKoladiya@users.noreply.github.com>
2022-12-26 15:02:34 +0530

    Add files via upload

---
+++ C-Language/syllabus_practical/PR10_2.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+union book
+{
+    char title[20];
+    char author[20];
+    float price;
+    int flag;
+    int accession;
+}b;
+void display(union book b)
+{
+    if(b.flag == 1)
+    {
+        printf("Your Book Issued...");
+    }
+    else
+    {
+        printf("Your Book not issued!!!");
+    }
+      
+}
+void main()
+{
+    printf("Enter accession number : ");
+    scanf("%d",&b.accession);
+    fflush(stdin);
+    printf("Enter book name : ");
+    gets(b.title);
+    printf("Enter Author name : ");
+    gets(b.author);
+    printf("Enter price of book : ");
+    scanf("%f",&b.price);
+    printf("Enter 0 or 1 : ");
+    scanf("%d",&b.flag);
+    display(b);
+}
