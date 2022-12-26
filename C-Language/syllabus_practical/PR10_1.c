ceac27f4ff1196aea4ee7e643cafd6bae79b9cae
Vikas Koladiya <114695020+VikasKoladiya@users.noreply.github.com>
2022-12-26 14:37:58 +0530

    Add files via upload

---
+++ C-Language/syllabus_practical/PR10_1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+struct book
+{
+    char title[20];
+    char author[20];
+    float price;
+}b;
+void display(struct book b)
+{
+    printf("Book name is : %s",b.title);
+    printf("\nAuthor name is : %s",b.author);
+    printf("\nPrice of book is : %f",b.price);
+}
+void main()
+{
+    printf("Enter book name : ");
+    gets(b.title);
+    printf("Enter Author name : ");
+    gets(b.author);
+    printf("Enter price of book : ");
+    scanf("%f",&b.price);
+    display(b);
+}
